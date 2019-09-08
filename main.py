36ddc4fc8d2c8385f4bac1c7a6543dbe871429f0
Xalor90 <xalor90@outlook.com>
2019-09-07 22:40:34 -0700

    Got the bot working and stored the bot token as a constant hidden from source control

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+
+from src.passwords import get_bot_token
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(get_bot_token())
